5140fa5952ed59eafdd4891395790724eebff5bb
bandit <707316053@qq.com>
2019-05-24 14:21:06 +0800

    添加建筑消防设施软件版本实体类

---
+++ src/main/java/com/jugan/entity/type/BuildSoftware.java
@@ -0,0 +1,37 @@
+package com.jugan.entity.type;
+
+import lombok.Data;
+
+/**
+ * 建筑消防设施软件版本
+ * 共四个字节
+ * 编号 < 5 >
+ * @Author CL
+ * @Date 2019/5/24-14:12
+ */
+@Data
+public class BuildSoftware {
+    /**
+     * 系统类型标志
+     * (1个字节)
+     */
+    private int systemType;
+
+    /**
+     * 系统地址
+     * (1个字节)
+     */
+    private int systemAddress;
+
+    /**
+     * 主要版本
+     * (1个字节)
+     */
+    private int mainVersion;
+
+    /**
+     * 次要版本
+     * (1个字节)
+     */
+    private int secondary;
+}
