e400c9a56f6b8a3f3b5d5b01f61918f3e5cb0e9f
bandit <707316053@qq.com>
2019-05-24 15:19:39 +0800

    用户信息传输装置软件版本

---
+++ src/main/java/com/jugan/entity/upBound/UserSoftware.java
@@ -0,0 +1,25 @@
+package com.jugan.entity.upBound;
+
+import lombok.Data;
+
+/**
+ * 用户信息传输装置软件版本
+ * 共 2 个字节
+ * 编号< 25 >
+ * @Author CL
+ * @Date 2019/5/24-15:18
+ */
+@Data
+public class UserSoftware {
+    /**
+     * 主要版本
+     * (1个字节)
+     */
+    private int mainVersion;
+
+    /**
+     * 次要版本
+     * (1个字节)
+     */
+    private int secondary;
+}
